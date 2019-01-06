07ff370096849612d4d638b774c0275a3aa1f578
Sasha Volokh <sasha.volokh@gmail.com>
2019-01-05 22:18:27 -0800

    Fixed resources.cpp for generic platform.

--- podge/os/generic/resources.cpp
+++ podge/os/generic/resources.cpp
@@ -1,29 +1,31 @@
+#define BOOST_SCOPE_EXIT_CONFIG_USE_LAMBDAS
 #include "../resources.hpp"
 
-#include <fstream>
-#include <experimental/filesystem>
+#include <boost/scope_exit.hpp>
 
-namespace fs = std::experimental::filesystem;
+#include <fstream>
+#include <dirent.h>
 
 namespace podge {
 
 std::string get_resource(const std::string &path) {
-	std::ifstream ifs(path, std::ios::binary);
-	return std::string(std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>());
+    std::ifstream ifs(path, std::ios::binary);
+    return std::string(std::istreambuf_iterator<char>(ifs), std::istreambuf_iterator<char>());
 }
 
 std::vector<std::string> list_resources(const std::string &path) {
-	std::vector<std::string> res;
-	fs::path p(path);
-	for(const auto &p : fs::directory_iterator(fs::path(path))) {
-		res.emplace_back(p.filename().string());
-	}
-	return res;
+    std::vector<std::string> res;
+    auto dp(opendir(path.c_str()));
+    struct dirent *ent;
+    while((ent = readdir(dp)) != nullptr) {
+        res.emplace_back(ent->d_name);
+    }
+    return res;
 }
 
 bool resource_exists(const std::string &path) {
-	fs::path p(path);
-	return fs::exists(p);
+    std::ifstream ifs(path, std::ios::binary);
+    return !ifs.fail();
 }
 
 }
