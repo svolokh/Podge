9a11b451b61aad1b7ad1c5d335bb68b5e2dfed32
Sasha Volokh <sasha.volokh@gmail.com>
2019-01-12 23:44:26 -0800

    Added iOS files.

---
+++ podge/os/ios/resources.mm
@@ -0,0 +1,21 @@
+#define BOOST_SCOPE_EXIT_CONFIG_USE_LAMBDAS
+
+#include "../../common.hpp"
+#include "../resources.hpp"
+
+#include <SDL.h>
+#include <SDL_system.h>
+#include <boost/scope_exit.hpp>
+
+namespace podge {
+
+std::string get_resource(const std::string &path) {
+}
+
+std::vector<std::string> list_resources(const std::string &path) {
+}
+
+bool resource_exists(const std::string &path) {
+}
+
+}
