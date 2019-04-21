d5d308c97148544d4b81c7385a194406dab00a69
Sasha Volokh <sasha.volokh@gmail.com>
2019-04-20 20:49:43 -0700

    Added orbit indicators for waffles.

---
+++ assets/tilesets/orbit_indicators.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="orbit_indicators" tilewidth="64" tileheight="64" tilecount="2" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0" type="basic">
+  <properties>
+   <property name="keyframes" type="file" value="orbit_indicators/generic.json"/>
+  </properties>
+  <image width="64" height="64" source="orbit_indicators/generic.png"/>
+ </tile>
+ <tile id="1" type="basic">
+  <properties>
+   <property name="keyframes" type="file" value="orbit_indicators/rubber.json"/>
+  </properties>
+  <image width="64" height="64" source="orbit_indicators/rubber.png"/>
+ </tile>
+</tileset>
